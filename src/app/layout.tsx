b2fd3cca78848f079fb9aea096c47993cfaca9fe
keerthanar09 <keerthana241495@gmail.com>
2025-09-05 13:48:59 +0530

    Updated resume

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <nav className="bg-linear-to-t from-grey-500 to-sky-500 shadow p-4 flex justify-center space-x-6 text-white">
           <Link href="/">Home</Link>
-          <Link href="/research">Blogs and Articles</Link>
+          <Link href="/research">Blogs, Articles and Reports</Link>
           <Link href="/projects">Projects</Link>
           <Link href="/contact">Contact</Link>
         </nav>
